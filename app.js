4954b01a6a222a1e472e2173390eff76ba8aaae1
Lilii <lilii-ann.older@voco.ee>
2022-11-11 12:55:13 +0200

    Populated data structure items to list

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const ItemCtrl = (function (){
     }
 
     return {
+        getItems: function (){
+            return data.items
+        },
         logData: function (){
             return data
         }
@@ -26,7 +29,23 @@ const ItemCtrl = (function (){
 
 // UI Controller
 const UICtrl = (function (){
-    console.log('UICtrl')
+    return {
+        populateItemList: function (items){
+            let html = '';
+
+            items.forEach(function(item){
+                html += `<li class="collection-item" id="item-${item.id}">
+        		<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        		<a href="#" class="secondary-content">
+          			<i class="edit-item fa fa-pencil"></i>
+        		</a>
+      			</li>`;
+            });
+
+            // insert list items
+            document.querySelector("#item-list").innerHTML = html;
+        }
+    }
 })();
 
 // App Controller
@@ -34,6 +53,8 @@ const App = (function(ItemCtrl, UICtrl){
     return {
         init: function (){
             console.log('Initializing App')
+            const items = ItemCtrl.getItems()
+            UICtrl.populateItemList(items)
         }
     }
 })(ItemCtrl, UICtrl);
