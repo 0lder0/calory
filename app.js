1ddac25e974e2f308685046c07f4255f551bbbf8
Lilii <lilii-ann.older@voco.ee>
2022-11-11 15:14:45 +0200

    Added item to data structure

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const ItemCtrl = (function (){
     const data = {
         items: [
             {id: 0, name: 'Steak Dinner', calories: 1200},
-            {id: 0, name: 'Cookie', calories: 400},
-            {id: 0, name: 'Eggs', calories: 300},
+            {id: 1, name: 'Cookie', calories: 400},
+            {id: 2, name: 'Eggs', calories: 300},
         ],
         total: 0
     }
@@ -20,6 +20,18 @@ const ItemCtrl = (function (){
     return {
         getItems: function (){
             return data.items
+        },
+        addItem: function (name, calories){
+            let ID;
+            if (data.items.length > 0){
+                ID = data.items[data.items.length - 1].id + 1
+            } else {
+                ID = 0
+            }
+            calories = parseInt(calories);
+            newItem = new Item(ID, name, calories);
+            data.items.push(newItem);
+            return newItem
         },
         logData: function (){
             return data
@@ -30,7 +42,10 @@ const ItemCtrl = (function (){
 // UI Controller
 const UICtrl = (function (){
     const UISelectors = {
-        itemList: '#item-list'
+        itemList: '#item-list',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories',
+        addBtn: '.add-btn'
     }
     return {
         populateItemList: function (items){
@@ -47,17 +62,41 @@ const UICtrl = (function (){
 
             // insert list items
             document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getSelectors: function (){
+            return UISelectors;
+        },
+        getItemInput: function (){
+            return {
+                name:document.querySelector(UISelectors.itemNameInput).value,
+                calories:document.querySelector(UISelectors.itemCaloriesInput).value
+            }
         }
     }
 })();
 
 // App Controller
 const App = (function(ItemCtrl, UICtrl){
+    const loadEventListeners = function (){
+        const UISelectors = UICtrl.getSelectors();
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    }
+
+    const itemAddSubmit = function (event){
+        const input = UICtrl.getItemInput()
+        if (input.name !== '' && input.calories !== ''){
+            const newItem = ItemCtrl.addItem(input.name, input.calories)
+            console.log(newItem)
+        }
+        event.preventDefault()
+    }
+
     return {
         init: function (){
             console.log('Initializing App')
             const items = ItemCtrl.getItems()
             UICtrl.populateItemList(items)
+            loadEventListeners()
         }
     }
 })(ItemCtrl, UICtrl);
